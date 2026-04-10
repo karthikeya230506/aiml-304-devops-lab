cabdbe531f972c1296a030a6a2ff2e34e1234f6b
pds karthikeya <245123748304@mvsrec.edu.in>
2026-04-10 13:19:18 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 sum_result = num1 + num2
 
